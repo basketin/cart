a368a82444836b4d9cb635225ec2881c1df11907
Karim Mohamed <komicho1996@gmail.com>
2023-11-14 23:17:29 +0200

    Create ServiceProvider

---
+++ src/Providers/StorePHPCartServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Storephp\Cart\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StorePHPCartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
