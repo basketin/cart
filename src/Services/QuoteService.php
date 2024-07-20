d145372bebbdd36429b2f313620798e8200063ed
Karim Mohamed <komicho1996@gmail.com>
2024-07-20 11:49:35 +0300

    Fix: add `cart_id` to select a quotes

--- src/Services/QuoteService.php
+++ src/Services/QuoteService.php
@@ -17,7 +17,7 @@ class QuoteService
 
     public function addQuote(IQuote $item, $quantity = 1)
     {
-        if ($item->quote()->first()) {
+        if ($item->quote()->where('cart_id', $this->cart->id)->first()) {
             $this->increaseQuote($item, $quantity);
             return $this;
         }
@@ -32,7 +32,7 @@ class QuoteService
 
     public function increaseQuote(IQuote $item, $quantity = 1)
     {
-        if (!$_item = $item->quote()->first()) {
+        if (!$_item = $item->quote()->where('cart_id', $this->cart->id)->first()) {
             throw new QuoteNotFoundException;
         }
 
@@ -47,7 +47,7 @@ class QuoteService
 
     public function decreaseQuote(IQuote $item, $quantity = 1)
     {
-        if (!$_item = $item->quote()->first()) {
+        if (!$_item = $item->quote()->where('cart_id', $this->cart->id)->first()) {
             throw new QuoteNotFoundException;
         }
 
@@ -68,7 +68,7 @@ class QuoteService
 
     public function removeQuote(IQuote $item)
     {
-        if (!$_item = $item->quote()->first()) {
+        if (!$_item = $item->quote()->where('cart_id', $this->cart->id)->first()) {
             throw new QuoteNotFoundException;
         }
 
