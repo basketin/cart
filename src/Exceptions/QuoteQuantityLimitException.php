c2f9ccf56d2ad09a86735d5b0450451b4f4527a6
Karim Mohamed <komicho1996@gmail.com>
2026-03-06 00:39:35 +0200

    Refactor: update exception constructors to use nullable type hinting for previous throwable

--- src/Exceptions/QuoteQuantityLimitException.php
+++ src/Exceptions/QuoteQuantityLimitException.php
@@ -3,10 +3,11 @@
 namespace Obelaw\Basketin\Cart\Exceptions;
 
 use Exception;
+use Throwable;
 
 class QuoteQuantityLimitException extends Exception
 {
-    public function __construct($message = 'You exceed the existing limit and the quantity cannot be increased', $code = 0, Throwable $previous = null)
+    public function __construct($message = 'You exceed the existing limit and the quantity cannot be increased', $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
